f98570441478eebf8adab9e3c820b26b53eddb60
APPELLEJAX <midnbeal18@gmail.com>
2016-03-05 10:55:44 -0500

    has database functionality for our types

--- lnf/config/routes.rb
+++ lnf/config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :articles
+
+  resources :founds
+  resources :losts
+
   root 'welcome#index'
+
 
   # You can have the root of your site routed with "root"
 
